7ae3a5d029132d7bb635f5f906a82bf907f98040
Gurpreet Atwal <gurpreetatwal@users.noreply.github.com>
2016-06-11 22:56:43 -0700

    add router

---
+++ routes/router.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+// define the home page route
+router.get('/', function(req, res) {
+  res.send('home page');
+});
+
+module.exports = router;
