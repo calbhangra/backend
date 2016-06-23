0fc100ac0f404f4bca57a2369db8d6484de8b455
Gurpreet Atwal <gurpreetatwal@users.noreply.github.com>
2016-06-22 19:05:20 -0700

    create skeleton gig controller, and controllers index

---
+++ controllers/gig.controller.js
@@ -0,0 +1,17 @@
+var GigController = {};
+
+GigController.create = function(req, res) {
+  
+}
+
+GigController.get = function(req, res) {
+  
+}
+
+GigController.update = function(req, res) {
+
+}
+
+GigController.del = function(req, res) {
+  
+}
---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+var gig = require('./gig.controller.js');
+
+var controllers = {};
+controllers.gig = gig;
+
+module.exports = controllers;
