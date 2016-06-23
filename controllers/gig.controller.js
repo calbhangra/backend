bbfb20e8478ac99d52dba27534e143795ce87905
Gurpreet Atwal <gurpreetatwal@users.noreply.github.com>
2016-06-22 19:37:12 -0700

    spec out the gig controller

--- controllers/gig.controller.js
+++ controllers/gig.controller.js
@@ -1,17 +1,31 @@
 var GigController = {};
+var model = require('../models/gig.model.js');
 
 GigController.create = function(req, res) {
-  
+  // TODO
+  // 1. validate incoming data
+  // 2. send validated to model for creation 
 }
 
 GigController.get = function(req, res) {
-  
+  // TODO
+  // 1. determine if req for one object or multiple
+  // 2. check user permissions 
+  // 3. request object(s) from the model 
 }
 
 GigController.update = function(req, res) {
-
+  // TODO
+  // 1. validate incoming data
+  // 2. check user's permissions
+  // 3. send validated data to model for update
 }
 
 GigController.del = function(req, res) {
-  
+  // TODO
+  // 1. check user permissions
+  // 2. log username and metadata into seperate log file
+  // 3. send deletion request to model
 }
+
+export default GigController;
