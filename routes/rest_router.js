0d4478d98ae95fd1d66c8890a1c2c430eb4f0143
Gurpreet Atwal <gurpreetatwal@users.noreply.github.com>
2016-06-22 19:26:49 -0700

    Create rest router to handle basic rest routes

---
+++ routes/rest_router.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var express = require('express');
+
+/**
+ * Creates the 5 basic REST routes for a given controller. If custom routes are
+ * needed on top of the basic routes, they can be attached to returnd router object.
+ *
+ * @param {Object} controller the controller that contains logic for a route
+ * @return {express.Router} an instance of the express router with REST routes attached
+ */
+function restRouter(controller) {
+  const router = express.Router(); // eslint-disable-line new-cap
+
+  router.route('/')
+    .get(controller.get)
+    .post(controller.create);
+
+  router.route('/:id')
+    .get(controller.get)
+    .put(controller.update)
+    .delete(controller.del);
+
+  if (controller.middleware) {
+    router.param('id', controller.middleware);
+  }
+
+  return router;
+}
+
+export default restRouter;
--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,14 @@
 
 var express = require('express');
 var router = express.Router(); // eslint-disable-line new-cap
+var restRouter = require('./rest_router');
+var controllers = require('../controllers');
 
 // define the home page route
 router.get('/', function index(req, res) {
   res.send('home page');
 });
+
+router.get('/gig', restRouter(controllers.gig));
 
 module.exports = router;
