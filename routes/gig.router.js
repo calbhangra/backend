b0bcb6eca224587404cfbca2961f053991ccc1eb
Gurpreet Atwal <gurpreetatwal@users.noreply.github.com>
2016-08-20 14:26:34 -0700

    finish first draft of gig routes
    still need to:
    - handle validation errors better
    - check permissions

--- routes/gig.router.js
+++ routes/gig.router.js
@@ -2,43 +2,62 @@
 
 import express from 'express';
 import models from '../models';
+import {ValidationError} from 'sequelize';
 
 var Gig = models.Gig;
 var router = express.Router(); // eslint-disable-line new-cap
 
+// TODO add permission checks to all routes except create
+// TODO handle all validation errors in one place
+
 router.get('/', (req, res) => {
-  // TODO
-  // 1. determine if req for one object or multiple
-  // 2. check user permissions
-  // 3. request object(s) from the model
+
   Gig.findAll()
     .then(data => res.send(data));
 });
 
 router.post('/', (req, res) => {
-  // TODO
-  // 1. validate incoming data
-  // 2. send validated to model for creation
+
   Gig.create(req.body)
-    .then(data => res.send(data));
+    .then(data => res.send(data))
+    .catch(ValidationError, err => {
+      res.status(400).send(err.errors[0]);
+     })
 });
 
-router.put('/:gigId', (req, res) => {
-  // TODO
-  // 1. validate incoming data
-  // 2. check user's permissions
-  // 3. send validated data to model for update
-  // eslint-disable-next-line no-magic-numbers
-  res.sendStatus(500);
+router.get('/:id', (req, res) => {
+
+  Gig.findById(req.params.id)
+    .then(gig => res.send(gig));
 });
 
-router.delete('/:gigId', (req, res) => {
-  // TODO
-  // 1. check user permissions
-  // 2. log username and metadata into seperate log file
-  // 3. send deletion request to model
-  // eslint-disable-next-line no-magic-numbers
-  res.sendStatus(500);
+router.put('/:id', (req, res) => {
+
+  Gig.update(req.body, {
+      where: {id: req.params.id},
+      returning: true,
+     })
+    .then(([count, records])=> {
+      if (count !== 1) {
+        // TODO throw error instead
+        res.sendStatus(500);
+      }
+
+      res.send(records[0]);
+     })
+    .catch(ValidationError, err => {
+      res.status(400).send(err.errors[0]);
+     })
+});
+
+router.delete('/:id', (req, res) => {
+  // TODO log username and metadata into seperate log file
+
+  Gig.destroy({where: {id: req.params.id}})
+    .then(count => {
+      var status = count === 1 ? 200 : 500;
+      res.sendStatus(status);
+    });
 });
 
 export default router;
