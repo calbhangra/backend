9425f60fe3ab760ed9d41769759824c59dc8428b
Gurpreet Atwal <gurpreetatwal@users.noreply.github.com>
2016-09-28 23:08:27 -0700

    finish writing tests for authenticate.jwt

--- test/middleware/authenticate.js
+++ test/middleware/authenticate.js
@@ -1,6 +1,8 @@
+import _ from 'lodash';
 import test from 'ava';
 import sinon from 'sinon';
 
+import jwt from '../../lib/jwt';
 import {User} from '../../models';
 import authenticate from '../../middleware/authenticate';
 import {AuthError, InvalidRequestError} from '../../lib/errors';
@@ -14,6 +16,46 @@ test.before(() => {
   });
 });
 
+test.cb('jwt throws AuthError if token is not provided in auth header', t => {
+
+  authenticate.jwt({get: _.noop}, {}, arg => {
+    t.true(arg instanceof AuthError);
+    t.true(_.includes(arg.message, 'JWT'));
+    t.true(_.includes(arg.message, 'Authorization'));
+    t.end();
+  });
+
+});
+
+test.cb('jwt throws JWTError if token is invalid', t => {
+
+  let req = {
+    get: sinon.stub().withArgs('authorization').returns('venus'),
+  };
+
+  authenticate.jwt(req, {}, arg => {
+    t.true(arg instanceof jwt.JsonWebTokenError);
+    t.end();
+  });
+
+});
+
+test.cb('jwt sets req.userId and req.roles', t => {
+
+  const token = jwt.create({sub: 'jupiter', roles: ['giant']});
+  let req = {
+    get: sinon.stub().withArgs('authorization').returns(token),
+  };
+
+
+  authenticate.jwt(req, {}, () => {
+    t.is(req.userId, 'jupiter');
+    t.is(req.roles[0], 'giant');
+    t.end();
+  });
+
+});
+
 test('password fails when email or password is missing', t => {
 
   let stub = sinon.stub();
@@ -24,6 +66,7 @@ test('password fails when email or password is missing', t => {
 
   t.is(stub.callCount, 3);
   stub.args.forEach(args => t.true(args[0] instanceof InvalidRequestError));
+
 });
 
 test.cb('password throws AuthError if user is not found', t => {
@@ -37,6 +80,7 @@ test.cb('password throws AuthError if user is not found', t => {
     t.true(arg instanceof AuthError);
     t.end();
   });
+
 });
 
 test.cb("password throws AuthError if password's don't match", t => {
@@ -50,6 +94,7 @@ test.cb("password throws AuthError if password's don't match", t => {
     t.true(arg instanceof AuthError);
     t.end();
   });
+
 });
 
 test.cb('password returns a JWT on success', t => {
@@ -66,4 +111,5 @@ test.cb('password returns a JWT on success', t => {
   };
 
   authenticate.password({body}, {json});
+
 });
