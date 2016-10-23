d63e3dc2a748c47ffbeedd8e820a82984369dc58
Gurpreet Atwal <gurpreetatwal@users.noreply.github.com>
2016-10-23 15:31:04 -0700

    remove poor usages of t.plan in jwt tests and reword test titles

--- test/lib/jwt.js
+++ test/lib/jwt.js
@@ -10,7 +10,7 @@ test('key length matches hash length of algorithm', t => {
   t.is(secret.toString('ascii').length, hashLengthInBytes);
 });
 
-test('create works', t => {
+test('create', t => {
   const token = jwt.decode(jwt.create({
     sub: 'override',
     foo: 'blue',
@@ -22,18 +22,12 @@ test('create works', t => {
   t.is(token.foo, 'blue');
 });
 
-test('create wraps any jwt.sign errors into JsonWebTokenErrors', t => {
-  t.plan(2);
-
-  try {
-    jwt.create('not an object');
-  } catch (e) {
-    t.true(e instanceof jwt.JsonWebTokenError);
-    t.is(e.name, 'JsonWebTokenError');
-  }
+test('create - wraps errors into JsonWebTokenErrors', t => {
+  const err = t.throws(() => jwt.create('blah'), jwt.JsonWebTokenError);
+  t.is(err.name, 'JsonWebTokenError');
 });
 
-test('create sets expiration to 6 hours', t => {
+test('create - sets expiration to 6 hours', t => {
   const token = jwt.decode(jwt.create());
   const SIX_HOURS = 60 * 60 * 6;
 
@@ -41,7 +35,7 @@ test('create sets expiration to 6 hours', t => {
   t.is(token.exp, Math.floor(new Date() / 1000) + SIX_HOURS);
 });
 
-test('verify works', t => {
+test('verify', t => {
   const body = {sub: 'test', pub: 'pizza'};
   const decoded = jwt.verify(jwt.create(body));
 
@@ -49,12 +43,10 @@ test('verify works', t => {
   t.is(decoded.sub, body.sub);
 });
 
-test('verify for custom options', t => {
-  t.plan(1);
+test('verify - custom options override defaults', t => {
+  const token = jwt.create();
 
-  try {
-    jwt.verify(jwt.create(), {algorithms: ['garbage']});
-  } catch (e) {
-    t.true(e instanceof jwt.JsonWebTokenError);
-  }
+  t.throws(function() {
+    jwt.verify(token, {algorithms: ['override not a valid algo']});
+  });
 });
